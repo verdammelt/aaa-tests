1f323e1234db4851281863f00148d3897a098039
Mark Simpson <verdammelt@gmail.com>
2018-04-20 16:24:26 -0400

    Add WebPack for building code.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+
+  entry: './src/index',
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+
+        options: {
+          presets: ['env']
+        }
+      }
+    ]
+  }
+};
